85dd7dc99877bad2c0411d7ff94989b9e1bd17c9
Chen <ChenRaptor@outlook.fr>
2023-02-25 15:51:36 +0100

    Ajout de la logique pour générer des astres

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import genspace from '../libs/SpaceGenerator'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  
+  useEffect(() => {
+    genspace()
+  },[])
+
   return (
     <>
       <Head>
